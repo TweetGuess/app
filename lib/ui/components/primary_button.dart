43c2fae36e5893153fc42b7ffa5192b62762c49b
Flawn <fl4wn@protonmail.com>
2023-06-11 17:36:12 +0200

    💄 Add skeleton button for primary game answer button

---
+++ lib/ui/components/primary_button.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class UIPrimaryButton extends StatefulWidget {
+  const UIPrimaryButton({super.key});
+
+  @override
+  State<UIPrimaryButton> createState() => UIPrimaryButtonState();
+}
+
+class UIPrimaryButtonState extends State<UIPrimaryButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
