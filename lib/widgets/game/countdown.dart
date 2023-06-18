a23e09feb56664db09883b39d831f299e49a0a8a
Flawn <fl4wn@protonmail.com>
2023-06-18 23:22:18 +0200

    💄 Implement proper pre-game countdown
    looks kinda lit

--- lib/widgets/game/countdown.dart
+++ lib/widgets/game/countdown.dart
@@ -1,11 +1,10 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tweetguess/core/bloc/game/game_event.dart';
 
 import '../../core/bloc/game/game_bloc.dart';
+import '../../ui/text/countdown_text.dart';
 import 'game.dart';
 
 class Countdown extends StatefulWidget {
@@ -15,39 +14,71 @@ class Countdown extends StatefulWidget {
   State<Countdown> createState() => _CountdownState();
 }
 
-class _CountdownState extends State<Countdown> {
-  late Timer _timer;
+class _CountdownState extends State<Countdown>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _fadeInAnimation;
+  late Animation<Offset> _slideInLeftAnimation;
+  late Animation<Offset> _slideInRightAnimation;
+
   int _count = 3;
 
   @override
   void initState() {
     super.initState();
+
+    _setupAnimations();
+
     startTimer();
+  }
+
+  void _setupAnimations() {
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1000),
+    );
+
+    _fadeInAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: const Interval(
+          0.0,
+          0.400,
+          curve: Curves.ease,
+        ),
+      ),
+    );
+    _slideInLeftAnimation = Tween<Offset>(
+      begin: const Offset(1.0, 0.0),
+      end: Offset.zero,
+    ).animate(_controller);
+
+    _slideInRightAnimation = Tween<Offset>(
+      begin: const Offset(-1.0, 0.0),
+      end: Offset.zero,
+    ).animate(_controller);
   }
 
   @override
   void dispose() {
+    _controller.dispose();
     super.dispose();
-    _timer.cancel();
   }
 
-  void startTimer() {
-    const oneSec = Duration(seconds: 1);
-    _timer = Timer.periodic(
-      oneSec,
-      (Timer timer) {
-        if (_count == 1) {
-          setState(() {
-            timer.cancel();
-            _count = 0;
-          });
-        } else {
-          setState(() {
-            _count--;
-          });
-        }
-      },
-    );
+  void startTimer() async {
+    await Future.delayed(const Duration(milliseconds: 500));
+    _controller.forward();
+    _controller.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        setState(() {
+          _count--;
+          if (_count > 0) {
+            _controller.reset();
+            _controller.forward();
+          }
+        });
+      }
+    });
   }
 
   @override
@@ -65,12 +96,36 @@ class _CountdownState extends State<Countdown> {
       }
     });
 
-    return Container(
-      color: Colors.blue,
+    return Material(
       child: Container(
         color: Colors.blue,
         child: Center(
-          child: Text(_count.toString()),
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              if (_count == 3)
+                FadeTransition(
+                  opacity: _fadeInAnimation,
+                  child: const CountdownText(
+                    '3',
+                  ),
+                ),
+              if (_count == 2)
+                SlideTransition(
+                  position: _slideInLeftAnimation,
+                  child: const CountdownText(
+                    '2',
+                  ),
+                ),
+              if (_count <= 1)
+                SlideTransition(
+                  position: _slideInRightAnimation,
+                  child: const CountdownText(
+                    '1',
+                  ),
+                ),
+            ],
+          ),
         ),
       ),
     );
