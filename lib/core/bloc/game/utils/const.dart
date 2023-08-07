20b8ab6a9861fcd7d98c5bccde411068599043b7
Flawn <fl4wn@protonmail.com>
2023-08-08 01:57:02 +0300

    🔧 GameConstants: Increase amount of lives to 5
    Due to changes in game logic

--- lib/core/bloc/game/utils/const.dart
+++ lib/core/bloc/game/utils/const.dart
@@ -1,8 +1,8 @@
 class GameConstants {
   static const int TIME_PER_ROUND = 30;
-  static const int MAX_LIVES = 3;
+  static const int MAX_LIVES = 5;
   static const int MINUS_POINTS = -15;
 
-  // Specific event codes passed through the game 
+  // Specific event codes passed through the game
   static const int NO_TIME_LEFT_EVENT = -1;
 }
