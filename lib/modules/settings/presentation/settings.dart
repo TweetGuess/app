f0cd06aded65a3196adb03f27c82a48c037f2e6f
Flawn <fl4wn@protonmail.com>
2023-08-12 01:43:21 +0300

    ✨ (Settings): Add rudimentary first draft
    so I can reset my stats LOL

--- lib/modules/settings/presentation/settings.dart
+++ lib/modules/settings/presentation/settings.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:tweetguess/core/bloc/user/user_bloc.dart';
+import 'package:tweetguess/core/bloc/user/user_event.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.red);
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: const Text("Settings"),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ListTile(
+            title: const Text('Reset Statistics'),
+            onTap: () {
+              GetIt.I<UserBloc>().add(UserResetStats());
+            },
+          ),
+          ListTile(
+            title: const Text('Change App Appearance'),
+            onTap: () {
+              // TODO: Implement change app appearance functionality
+            },
+          ),
+          ListTile(
+            title: const Text('Change App Language'),
+            onTap: () {
+              // TODO: Implement change app language functionality
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
