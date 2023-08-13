9618cac56946c46010483a3a533bc52fd8a2ee06
Flawn <fl4wn@protonmail.com>
2023-08-13 15:55:37 +0300

    💄 (UIPrimaryBottomSheet): Move settings bottom sheet to own componenet

---
+++ lib/ui/components/primary_bottom_sheet.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class UIPrimaryBottomSheet extends StatelessWidget {
+  const UIPrimaryBottomSheet({super.key, required this.child});
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+
+    return Container(
+      decoration: BoxDecoration(
+        color: colorScheme.surface,
+        borderRadius: const BorderRadius.vertical(top: Radius.circular(16)),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              width: 48,
+              height: 4,
+              decoration: BoxDecoration(
+                color: colorScheme.onSurface.withOpacity(0.3),
+                borderRadius: BorderRadius.circular(2),
+              ),
+            ),
+            const SizedBox(height: 16),
+            child,
+          ],
+        ),
+      ),
+    );
+  }
+}
