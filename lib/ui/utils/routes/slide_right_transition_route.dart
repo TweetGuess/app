ef0d59fd2d8532a8093dc85e5475d862aa89be10
Flawn <fl4wn@protonmail.com>
2023-12-31 01:15:46 +0100

    💄 (Animations): Add SlightRideTransiton
    this works exactly like a mf ios transition don't tell me why

---
+++ lib/ui/utils/routes/slide_right_transition_route.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/cupertino.dart';
+import 'package:tweetguess/ui/utils/transitions/helper.dart';
+
+class SlideRightTransition extends PageRouteBuilder {
+  final Widget page;
+  SlideRightTransition({
+    required this.page,
+    Offset? offset,
+    Duration? transitionDuration,
+    RouteSettings? settings,
+  }) : super(
+          pageBuilder: (
+            BuildContext context,
+            Animation<double> animation,
+            Animation<double> secondaryAnimation,
+          ) =>
+              page,
+          transitionsBuilder: (
+            context,
+            animation,
+            secondaryAnimation,
+            child,
+          ) {
+            return TransitionHelper.slideRightTransition(
+              context,
+              animation,
+              secondaryAnimation,
+              child,
+            );
+          },
+          transitionDuration:
+              transitionDuration ?? const Duration(milliseconds: 600),
+          settings: settings,
+        );
+}
