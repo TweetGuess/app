eb43b15acd12416732fac892879346789f04ee7f
Flawn <fl4wn@protonmail.com>
2024-07-03 00:52:16 +0200

    🐛 (install.sh): Change to-be-checkedout Flutter version

--- deployment/build.sh
+++ deployment/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-BUILD_COMMAND="flutter build web --release --web-renderer canvaskit"
+BUILD_COMMAND="flutter/bin/flutter build web --release --web-renderer canvaskit"
 echo $BUILD_COMMAND
 eval $BUILD_COMMAND 
