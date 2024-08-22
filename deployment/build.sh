665bc569c3cde969cdac7051a7bfd2c1557796f3
Flawn <fl4wn@protonmail.com>
2024-08-22 14:22:10 +0200

    ♻️ (Build Script): Colorcode & prefix logs
    Helpful for CI/CD env

--- deployment/build.sh
+++ deployment/build.sh
@@ -11,14 +11,26 @@ enable_analytics=$ENABLE_ANALYTICS
 env_suffix=$([[ "$deployment_environment" == "PROD" ]] && echo ".prod" || echo ".dev")
 env_file_name="env/.env${env_suffix}"
 
+# Define color codes
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+# Log function
+log() {
+  echo -e "${GREEN}BUILD-SCRIPT: $1${NC}"
+}
+
+log "Writing environment variables to $env_file_name"
 echo "SENTRY_DSN=$sentry_dsn" >> $env_file_name
 echo "ENVIRONMENT=$deployment_environment" >> $env_file_name
 echo "ENABLE_ANALYTICS=$enable_analytics" >> $env_file_name
 
 # Setup environment file
 FB_COMMAND="flutter/bin/dart run build_runner build --delete-conflicting-outputs"
-echo $FB_COMMAND && eval $FB_COMMAND 
+log "Running build_runner: $FB_COMMAND"
+eval $FB_COMMAND 
 
 # Build the project
 BUILD_COMMAND="flutter/bin/flutter build web --release --web-renderer canvaskit"
-echo $BUILD_COMMAND && eval $BUILD_COMMAND 
+log "Building the project: $BUILD_COMMAND"
+eval $BUILD_COMMAND
