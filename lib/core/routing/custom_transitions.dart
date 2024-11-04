b20958304b6687b2bfc2c3d870f1bba686cb5df2
Flawn <fl4wn@protonmail.com>
2024-11-04 11:08:40 +0100

    💫 (Transitions): Fix transition issues with go_router

--- lib/core/routing/custom_transitions.dart
+++ lib/core/routing/custom_transitions.dart
@@ -1,87 +1,123 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:tweetguess/core/ui/observers/navigator.dart';
 import 'package:tweetguess/ui/utils/transitions/helper.dart';
 
+enum TransitionType {
+  none,
+  slideRight,
+  circularReveal,
+  gameEntry,
+  nextRound,
+}
+
 class CustomTransitions {
-  // Slide right transition
-  static CustomTransitionPage<T> slideRightTransition<T>({
+  static CustomTransitionPage<T> createTransition<T>({
     required Widget child,
-    String? name,
-    Object? arguments,
-    Duration duration = const Duration(milliseconds: 600),
-  }) {
-    return CustomTransitionPage<T>(
-      child: child,
-      name: name,
-      arguments: arguments,
-      transitionsBuilder: (context, animation, secondaryAnimation, child) {
-        return TransitionHelper.slideRightTransition(
-          context,
-          animation,
-          secondaryAnimation,
-          child,
-        );
-      },
-      transitionDuration: duration,
-    );
-  }
-
-  // Circular reveal transition
-  static CustomTransitionPage<T> circularRevealTransition<T>({
-    required Widget child,
+    required TransitionType type,
     String? name,
     Object? arguments,
     Offset? offset,
     Duration duration = const Duration(milliseconds: 600),
+    LocalKey? key,
   }) {
-    return CustomTransitionPage<T>(
-      child: child,
-      name: name,
-      arguments: arguments,
-      transitionsBuilder: (context, animation, secondaryAnimation, child) {
-        return TransitionHelper.circularRevealTransition(
-          context,
-          animation,
-          secondaryAnimation,
-          child,
-          offset,
+    switch (type) {
+      case TransitionType.none:
+        return NoTransitionPage<T>(
+          child: child,
+          name: name,
+          arguments: arguments,
+          key: key,
         );
-      },
-      transitionDuration: duration,
-    );
-  }
-
-  // Game entry transition
-  static CustomTransitionPage<T> gameEntryTransition<T>({
-    required Widget child,
-    String? name,
-    Object? arguments,
-    Offset? offset,
-    Duration duration = const Duration(milliseconds: 600),
-  }) {
-    return CustomTransitionPage<T>(
-      child: child,
-      name: name,
-      arguments: arguments,
-      transitionsBuilder: (context, animation, secondaryAnimation, child) {
-        if (animation.status == AnimationStatus.forward) {
-          return TransitionHelper.circularRevealTransition(
-            context,
-            animation,
-            secondaryAnimation,
-            child,
-            offset,
-          );
-        } else {
-          return TransitionHelper.slideRightTransition(
-            context,
-            animation,
-            secondaryAnimation,
-            child,
-          );
-        }
-      },
-      transitionDuration: duration,
-    );
+        
+      case TransitionType.slideRight:
+        return CustomTransitionPage<T>(
+          child: child,
+          name: name,
+          arguments: arguments,
+          key: key,
+          transitionsBuilder: (context, animation, secondaryAnimation, child) {
+            return TransitionHelper.slideRightTransition(
+              context,
+              animation,
+              secondaryAnimation,
+              child,
+            );
+          },
+          transitionDuration: duration,
+        );
+        
+      case TransitionType.circularReveal:
+        return CustomTransitionPage<T>(
+          child: child,
+          name: name,
+          arguments: arguments,
+          key: key,
+          transitionsBuilder: (context, animation, secondaryAnimation, child) {
+            return TransitionHelper.circularRevealTransition(
+              context,
+              animation,
+              secondaryAnimation,
+              child,
+              offset,
+            );
+          },
+          transitionDuration: duration,
+        );
+        
+      case TransitionType.gameEntry:
+        return CustomTransitionPage<T>(
+          child: child,
+          name: name,
+          arguments: arguments,
+          key: key,
+          transitionsBuilder: (context, animation, secondaryAnimation, child) {
+            if (animation.status == AnimationStatus.forward) {
+              return TransitionHelper.circularRevealTransition(
+                context,
+                animation,
+                secondaryAnimation,
+                child,
+                offset,
+              );
+            } else {
+              // What looks without sense makes sense as we only want the transition to happen when we exit the game
+              if (AppNavObserver.navStack.last.name != "/game/overview") {
+                return TransitionHelper.slideRightTransition(
+                  context,
+                  animation,
+                  secondaryAnimation,
+                  child,
+                );
+              } else {
+                return child;
+              }
+          }
+          },
+          transitionDuration: duration ,
+        );
+      
+      case TransitionType.nextRound:
+        return CustomTransitionPage<T>(
+          child: child,
+          name: name,
+          arguments: arguments,
+          key: key,
+          transitionsBuilder: (context, animation, secondaryAnimation, child) {
+            // What looks without sense makes sense as we only want the transition to happen when we exit the game
+            if (AppNavObserver.navStack.last.name != "/game/overview") {
+              return TransitionHelper.slideRightTransition(
+                context,
+                animation,
+                secondaryAnimation,
+                child,
+              );
+            } else {
+              return child;
+            }
+          },
+          transitionDuration: duration,
+        );
+    }
   }
 } 
--- lib/core/routing/router.dart
+++ lib/core/routing/router.dart
@@ -1,82 +1,112 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
 import 'package:go_router/go_router.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:tweetguess/core/bloc/user/user_bloc.dart';
 import 'package:tweetguess/core/routing/custom_transitions.dart';
+import 'package:tweetguess/core/ui/observers/navigator.dart';
 import 'package:tweetguess/modules/game/domain/models/game.dart';
 import 'package:tweetguess/modules/game/presentation/bloc/game_bloc.dart';
+import 'package:tweetguess/modules/game/presentation/game.dart';
 import 'package:tweetguess/modules/game/presentation/views/overview.dart';
 import 'package:tweetguess/modules/game/presentation/widgets/countdown.dart';
 import 'package:tweetguess/modules/home/presentation/home.dart';
 import 'package:tweetguess/modules/onboarding/presentation/intro.dart';
 import 'package:tweetguess/modules/profile/presentation/profile.dart';
 import 'package:tweetguess/modules/settings/presentation/settings.dart';
-import 'package:tweetguess/modules/game/presentation/game.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 final GlobalKey<NavigatorState> rootNavigatorKey = GlobalKey<NavigatorState>();
 
 final router = GoRouter(
   navigatorKey: rootNavigatorKey,
   initialLocation: '/',
+  observers: [
+    AppNavObserver(),
+    SentryNavigatorObserver(),
+    FirebaseAnalyticsObserver(analytics: FirebaseAnalytics.instance),
+  ],
   debugLogDiagnostics: true,
-  
   redirect: (BuildContext context, GoRouterState state) {
     final bool hasFinishedIntro = GetIt.I<UserBloc>().state.finishedIntro;
-    
+
     if (!hasFinishedIntro && state.matchedLocation != '/intro') {
       return '/intro';
     }
-    
+
     if (hasFinishedIntro && state.matchedLocation == '/intro') {
       return '/';
     }
-    
+
     return null;
   },
-
   routes: [
     GoRoute(
       path: '/',
-      pageBuilder: (context, state) => CustomTransitions.slideRightTransition(
-        child: const HomeScreen(),
-        name: 'home',
-      ),
+      builder: (context, state) => const HomeScreen(),
     ),
     GoRoute(
       path: '/intro',
-      pageBuilder: (context, state) => CustomTransitions.slideRightTransition(
+      pageBuilder: (context, state) => CustomTransitions.createTransition(
         child: const IntroScreen(),
+        type: TransitionType.slideRight,
         name: 'intro',
+        key: state.pageKey,
       ),
     ),
     GoRoute(
       path: '/settings',
-      pageBuilder: (context, state) => CustomTransitions.slideRightTransition(
-        child: const SettingsPage(),
-        name: 'settings',
-      ),
+      pageBuilder: (context, state) {
+        final Map<String, dynamic>? extra =
+            state.extra as Map<String, dynamic>?;
+        final TransitionType transitionType =
+            extra?['transition'] ?? TransitionType.circularReveal;
+
+        return CustomTransitions.createTransition(
+          child: const SettingsPage(),
+          type: transitionType,
+          offset: extra?['offset'] as Offset?,
+          name: 'settings',
+          key: state.pageKey,
+        );
+      },
     ),
     GoRoute(
       path: '/profile',
-      pageBuilder: (context, state) => CustomTransitions.slideRightTransition(
-        child: const ProfilePage(),
-        name: 'profile',
-      ),
+      pageBuilder: (context, state) {
+        final Map<String, dynamic>? extra =
+            state.extra as Map<String, dynamic>?;
+        final TransitionType transitionType =
+            extra?['transition'] ?? TransitionType.circularReveal;
+
+        return CustomTransitions.createTransition(
+          child: const ProfilePage(),
+          type: transitionType,
+          offset: extra?['offset'] as Offset?,
+          name: 'profile',
+          key: state.pageKey,
+        );
+      },
     ),
     GoRoute(
       path: '/game',
       pageBuilder: (context, state) {
-        final Map<String, dynamic>? extra = state.extra as Map<String, dynamic>?;
+        final Map<String, dynamic>? extra =
+            state.extra as Map<String, dynamic>?;
         final bool countdownEnabled = extra?['countdownEnabled'] ?? true;
         final GameBloc bloc = extra?['bloc'] ?? GameBloc();
-        
-        return CustomTransitions.gameEntryTransition(
+        final TransitionType transitionType =
+            extra?['transition'] ?? TransitionType.circularReveal;
+
+        return CustomTransitions.createTransition(
           child: BlocProvider<GameBloc>.value(
             value: bloc,
             child: countdownEnabled ? const Countdown() : const GameScreen(),
           ),
+          type: transitionType,
           name: 'game',
+          key: UniqueKey(),
         );
       },
       routes: [
@@ -84,22 +114,23 @@ final router = GoRouter(
           path: 'overview',
           pageBuilder: (context, state) {
             final Game game = state.extra as Game;
-            return CustomTransitions.circularRevealTransition(
+            return CustomTransitions.createTransition(
               child: BlocProvider.value(
                 value: context.read<GameBloc>(),
                 child: OverviewExitScreen(game: game),
               ),
+              type: TransitionType.circularReveal,
               name: 'game-overview',
+              key: state.pageKey,
             );
           },
         ),
       ],
     ),
   ],
-
   errorBuilder: (context, state) => Scaffold(
     body: Center(
       child: Text('Error: ${state.error}'),
     ),
   ),
-); 
+);
