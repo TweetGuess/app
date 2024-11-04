083d9ebd9565a6a75338b0640bf94aedf05263c1
Flawn <fl4wn@protonmail.com>
2024-11-04 10:48:39 +0100

    🗑️ (Wrapper): Change deprecated WillPop

--- lib/modules/game/presentation/widgets/helper/wrapper.dart
+++ lib/modules/game/presentation/widgets/helper/wrapper.dart
@@ -1,6 +1,7 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 
 import '../../bloc/game_bloc.dart';
 import '../../../domain/ui_controller/game_ui_controller.dart';
@@ -17,8 +18,8 @@ class GameWrapper extends StatelessWidget {
     return ValueListenableBuilder<bool>(
       valueListenable: controller.ignoringTapsNotifier,
       builder: (context, ignoringTaps, child) {
-        return WillPopScope(
-          onWillPop: () async {
+        return PopScope(
+          onPopInvokedWithResult: (bool didPop, Object? result) async {
             context.read<GameBloc>().add(PauseGame());
 
             // If we are not ignoring taps, and the onWillPop callback returns true then we want to exit the game.
@@ -28,8 +29,6 @@ class GameWrapper extends StatelessWidget {
               // If we are not leaving, start the game again.
               context.read<GameBloc>().add(StartGame());
             }
-
-            return false;
           },
           child: IgnorePointer(ignoring: ignoringTaps, child: child),
         );
@@ -39,7 +38,7 @@ class GameWrapper extends StatelessWidget {
   }
 
   Future<bool> _onWillPopCallback(BuildContext context) async {
-    return await showDialog(
+    return await showDialog<bool>(
       context: context,
       barrierDismissible: false,
       builder: (context) => AlertDialog(
@@ -47,17 +46,15 @@ class GameWrapper extends StatelessWidget {
         content: Text('game.exit-dialog.content'.tr()),
         actions: <Widget>[
           TextButton(
-            onPressed: () => Navigator.of(context).pop(false),
+            onPressed: () => context.pop(false),
             child: Text("global.no".tr()),
           ),
           TextButton(
-            onPressed: () {
-              Navigator.of(context).pop(true);
-            },
+            onPressed: () => context.pop(true),
             child: Text("global.yes".tr()),
           ),
         ],
       ),
-    );
+    ) ?? false;
   }
 }
